262831eb12751b0ab4265a9580f3e81038124a28
risingsunho <hsh205204@gmail.com>
2021-07-14 13:24:44 +0900

    Update UniversalFirebaseFunctionsModule.java

--- packages/functions/android/src/main/java/io/invertase/firebase/functions/UniversalFirebaseFunctionsModule.java
+++ packages/functions/android/src/main/java/io/invertase/firebase/functions/UniversalFirebaseFunctionsModule.java
@@ -1,30 +1,15 @@
 package io.invertase.firebase.functions;
 
-/*
- * Copyright (c) 2016-present Invertase Limited & Contributors
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this library except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *   http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- *
- */
-
 import android.content.Context;
-
 import com.facebook.react.bridge.ReadableMap;
 import com.google.android.gms.tasks.Task;
 import com.google.android.gms.tasks.Tasks;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.functions.FirebaseFunctions;
 import com.google.firebase.functions.HttpsCallableReference;
+import com.google.android.gms.tasks.Continuation;
+import com.google.firebase.functions.HttpsCallableResult;
+import androidx.annotation.NonNull;
 
 import io.invertase.firebase.common.UniversalFirebaseModule;
 
@@ -32,26 +17,25 @@ import java.util.concurrent.TimeUnit;
 
 @SuppressWarnings("WeakerAccess")
 public class UniversalFirebaseFunctionsModule extends UniversalFirebaseModule {
-  public static final String DATA_KEY = "data";
-  public static final String CODE_KEY = "code";
-  public static final String MSG_KEY = "message";
-  public static final String DETAILS_KEY = "details";
+public static final String DATA_KEY = "data";
+public static final String CODE_KEY = "code";
+public static final String MSG_KEY = "message";
+public static final String DETAILS_KEY = "details";
 
-  UniversalFirebaseFunctionsModule(Context context, String serviceName) {
-    super(context, serviceName);
-  }
+UniversalFirebaseFunctionsModule(Context context, String serviceName) {
+super(context, serviceName);
+}
 
-  Task<Object> httpsCallable(
-    String appName,
-    String region,
-    String origin,
-    String name,
-    Object data,
-    ReadableMap options
-  ) {
-    return Tasks.call(getExecutor(), () -> {
-      FirebaseApp firebaseApp = FirebaseApp.getInstance(appName);
-      FirebaseFunctions functionsInstance = FirebaseFunctions.getInstance(firebaseApp, region);
+Task<Object> httpsCallable(
+String appName,
+String region,
+String origin,
+String name,
+Object data,
+ReadableMap options
+) {
+FirebaseApp firebaseApp = FirebaseApp.getInstance(appName);
+FirebaseFunctions functionsInstance = FirebaseFunctions.getInstance(firebaseApp, region);
 
       HttpsCallableReference httpReference = functionsInstance.getHttpsCallable(name);
 
@@ -63,8 +47,15 @@ public class UniversalFirebaseFunctionsModule extends UniversalFirebaseModule {
         functionsInstance.useFunctionsEmulator(origin);
       }
 
-      return Tasks.await(httpReference.call(data)).getData();
+    Task<Object> task = httpReference.call(data).continueWith(new Continuation<HttpsCallableResult, Object>() {
+        @Override
+        public Object then(@NonNull Task<HttpsCallableResult> task) throws Exception {
+          Object result = task.getResult().getData();
+          return result;
+        }
     });
-  }
+    return task;
+
+}
 
 }
